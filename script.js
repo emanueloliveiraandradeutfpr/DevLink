36236f65570e3cfc527804da3f3b807d145ae7b2
Emanuel Oliveira <emanueloliveiraandrade.utfpr.edu.br>
2023-02-28 17:02:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+botao = document.getElementById("switch");
+
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle("light");
+
+    const img = document.querySelector("#profile img");
+
+    if (body.classList.contains("light")) {
+        img.setAttribute("src", "./assets/avatar-light.png");
+    } else {
+        img.setAttribute("src", "./assets/avatar.png");
+    }
+}
